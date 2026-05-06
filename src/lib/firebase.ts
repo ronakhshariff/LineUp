cea96a95eaa8980b09b1af85871932cc9ac02c37
ronakhshariff <ronakh.shariff@ucalgay.ca>
2026-05-05 22:33:30 -0600

    Configure Firebase with Line-Up project details for real-time sync

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set, onValue, update } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAm9ULbmtv2yWlA6n6p2kDsiW30xAtrBEc",
+  authDomain: "line-up-45179.firebaseapp.com",
+  databaseURL: "https://line-up-45179-default-rtdb.firebaseio.com",
+  projectId: "line-up-45179",
+  storageBucket: "line-up-45179.firebasestorage.app",
+  messagingSenderId: "648024848805",
+  appId: "1:648024848805:web:190cf75820fe924524d04c",
+  measurementId: "G-3N7FHKRP95"
 };
 
 // Initialize Firebase
